800d8f7ef1c4f4a21136892182d479c76f19aea1
Anupamachiluveri <chiluverianupama@gmail.com>
2024-12-13 18:59:16 +0530

    First Commit

--- Package/src/module-info.java
+++ Package/src/module-info.java
@@ -1,4 +1,5 @@
-/**
+/** package is small container which is there in java project.package is like folder 
+ * which is used to segregate class files. it is used to store duplicates files.
  * 
  */
 /**
